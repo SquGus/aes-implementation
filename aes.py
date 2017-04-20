b75aec7c53105cfacf6a65688044febb25846b07
SquGus <squgus@gmail.com>
2017-04-20 12:03:23 -0500

    Adds key function

--- aes.py
+++ aes.py
@@ -4,6 +4,22 @@ import base64
 
 BLOCK_SIZE = 16
 
+# Reads key and returns bytes of that key
+def read_key(keyname):
+	file = open(keyname, "rb")
+	key = base64.b16encode(file.read(BLOCK_SIZE))
+	if not key or len(key) < 32:
+		print("ERROR: Key is not large enough.")
+		return -1
+	return key
+
+
+# Expands key and returns expanded key
+def expand_key(key):
+	print("expand key")
+
+
+# Reads file and returns list with 16byte-blocks
 def read_file(filename):
 	file = open(filename, "rb")
 	fileBytes = [];
@@ -11,6 +27,7 @@ def read_file(filename):
 		block = base64.b16encode(file.read(BLOCK_SIZE))
 		if not block:
 			break
+		# Adds 01 and 00's to complete block
 		if len(block) < 32:
 			block += b'01'
 			while len(block) < 32:
@@ -19,17 +36,27 @@ def read_file(filename):
 	return fileBytes
 
 
-def main(filename): 
+# Main function that will call every stage of the encryption
+def cypher(byte, expandedKey):
+	print("BYTE -> " + byte.decode("utf-8"))
+
+def main(filename, keyfile):
+	name = filename
+	
+	key = read_key(keyfile)
+	if key == -1:
+		return
+
 	byteArray = read_file(filename)
 	for byte in byteArray:
-		print(byte)
+		byte = cypher(byte, 1)
 
 
 if __name__ == "__main__":
-	if (len(sys.argv) != 2):
-		print("Usage aes.py <file>")
+	if (len(sys.argv) != 3):
+		print("Usage aes.py <file> <key>")
 	else:
 		if os.path.isfile(sys.argv[1]):
-			main(sys.argv[1])
+			main(sys.argv[1], sys.argv[2])
 		else:
 			print("File does not exist")
